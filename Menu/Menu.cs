68971bb8e240ef0979525a0292687d296996b285
DPT263 <playpubg568@gmail.com>
2026-06-14 17:10:45 +0700

    Update Menu.cs

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -1,11 +1,63 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class Menu : MonoBehaviour
 {
+    [Header("Panels")]
+    [SerializeField] private GameObject caiDatPanel;
+
+    [Header("Audio")]
+    [SerializeField] private Slider volumeSlider;
+
+    private const string VolumeKey = "GameVolume";
+
+    private void Start()
+    {
+        if (caiDatPanel != null)
+        {
+            caiDatPanel.SetActive(false);
+        }
+
+        float savedVolume = PlayerPrefs.GetFloat(VolumeKey, 1f);
+        AudioListener.volume = savedVolume;
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = savedVolume;
+            volumeSlider.onValueChanged.AddListener(SetVolume);
+        }
+    }
+
     public void BatDau()
     {
         SceneManager.LoadScene("KhoiDau");
     }
+
+    public void MoCaiDat()
+    {
+        if (caiDatPanel != null)
+        {
+            caiDatPanel.SetActive(true);
+        }
+    }
+
+    public void DongCaiDat()
+    {
+        if (caiDatPanel != null)
+        {
+            caiDatPanel.SetActive(false);
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+
+        PlayerPrefs.SetFloat(VolumeKey, volume);
+        PlayerPrefs.Save();
+    }
+
     public void Thoat()
     {
         Application.Quit();
